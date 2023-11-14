38a8d46872567e428e52f9f5f11e93cdf1167de4
psk <psk@gmail.com>
2023-11-15 00:37:35 +0530

    C1

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,18 @@
+pipeline {
+    agent {
+        label {
+            label "slave-2"
+			customWorkspace "/mnt/slave-2"
+		}
+	}
+			
+	stages {
+        stage ("one") {
+            steps {
+                echo "This is branch master"
+			}
+						
+		}
+						
+	}
+}
